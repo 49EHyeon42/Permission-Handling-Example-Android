d27dcfbae873b81f86b5baff691fc15343f68f56
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-08-18 17:13:42 +0900

    feat: 권한 핸들링 로직 추가

--- app/src/main/java/dev/ehyeon/permissionhandlingexampleapplication/MainActivity.java
+++ app/src/main/java/dev/ehyeon/permissionhandlingexampleapplication/MainActivity.java
@@ -1,7 +1,16 @@
 package dev.ehyeon.permissionhandlingexampleapplication;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +19,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button permissionButton = findViewById(R.id.permission_button);
+
+        permissionButton.setOnClickListener(view -> {
+            if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED) {
+                requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permissions have been set.", Toast.LENGTH_SHORT).show();
+            } else {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO)) {
+                    Toast.makeText(this, "Permission is required.", Toast.LENGTH_SHORT).show();
+                } else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                            .setTitle("Request permission")
+                            .setMessage("Permissions are required to use this application.")
+                            .setNegativeButton("CANCEL", (dialog, which) -> {
+                            })
+                            .setPositiveButton("OK", (dialog, which) -> {
+                                Intent intent = new Intent(
+                                        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                        Uri.fromParts("package", getPackageName(), null));
+
+                                startActivity(intent); // Re-request to "intent"
+                            });
+
+                    AlertDialog alertDialog = builder.create();
+
+                    alertDialog.show();
+                }
+            }
+        }
     }
 }
